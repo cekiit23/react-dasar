069637f581506f185394d8a5be4db22a362e469a
cekiit23 <yogabenrc@gmail.com>
2021-07-31 20:00:24 +0700

    First commit

---
+++ src/Components/Classes/Beranda.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class Beranda extends React.Component {
+    render()  {
+        return <div><h1>Selamat datang di warung kopi</h1></div>
+    };
+}
+
+export default Beranda;
---
+++ src/Components/Functional/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Home = () => {
+    return <div>
+        <h2>Selamat datang diWebsite React.jS</h2>
+        
+    </div>
+}
+
+export default Home;
